8f04580198a3bd296e5bda731426f9bbf2fd682a
Paul Kazusek <paul.kazusek@gmail.com>
2020-09-03 19:57:11 +0200

    readable

--- canvas.js
+++ canvas.js
@@ -1,23 +1,17 @@
-// https://codeburst.io/creating-and-drawing-on-an-html5-canvas-using-javascript-93da75f001c1
-
-// https://github.com/CodeDraken/canvas-coords/blob/ae94cf92203f246362a3e4863c1361ae8d6bc482/src/Grid.js#L2
-
 class Rectangle {
 
-    constructor ( x = 0, y = 0, width = 0, height = 0, fillColor = '', strokeColor = '', strokeWidth = 2
-      ) {
+    constructor ( x = 0, y = 0, width = 0, height = 0, fillColor = '', strokeColor = '', strokeWidth = 2 ) {
         // ensure the arguments passed in are numbers
-        // a bit overkill for this tutorial
-        this.x = Number(x)
-        this.y = Number(y)
-        this.width = Number(width)
-        this.height = Number(height)
+        this.x = Number( x )
+        this.y = Number( y )
+        this.width = Number( width )
+        this.height = Number( height )
         this.fillColor = fillColor
         this.strokeColor = strokeColor
         this.strokeWidth = strokeWidth
       }
 
-      draw (ctx) {
+      draw ( ctx ) {
         // destructuring
         const { x, y, width, height, fillColor, strokeColor, strokeWidth } = this
     
@@ -32,7 +26,7 @@ class Rectangle {
         // create the *path*
         ctx.beginPath()
         ctx.strokeStyle = strokeColor
-        ctx.rect(x, y, width, height)
+        ctx.rect( x, y, width, height )
     
         // draw the path to screen
         ctx.fill()
@@ -47,7 +41,7 @@ class Rectangle {
 
 class Line {
 
-  constructor (startX, startY, endX, endY, color, lineWidth = 2 ) {
+  constructor ( startX, startY, endX, endY, color, lineWidth = 2 ) {
     this.color = color
     this.lineWidth = lineWidth
     this.startX = startX
@@ -56,7 +50,7 @@ class Line {
     this.endY = endY
   }
 
-  draw (ctx) {
+  draw ( ctx ) {
     const { startX, startY, endX, endY, color, lineWidth } = this
     
     ctx.save()
@@ -74,13 +68,13 @@ class Line {
 
 class Text {
 
-  constructor (text, x = 0, y = 0) {
-    this.text = String(text)
-    this.x = Number(x)
-    this.y = Number(y)
+  constructor ( text, x = 0, y = 0 ) {
+    this.text = String( text )
+    this.x = Number( x )
+    this.y = Number( y )
   }
 
-  draw (ctx) {
+  draw ( ctx ) {
     const { text, x, y } = this
 
     ctx.save()
@@ -89,7 +83,7 @@ class Text {
     ctx.textAlign = "center";
     ctx.fillStyle = "#000000";
     ctx.font = "bold 14px Arial";
-    ctx.fillText(text, x, y);
+    ctx.fillText( text, x, y );
 
     ctx.restore()
   }
@@ -97,8 +91,8 @@ class Text {
 
 class BarChart {
 
-  constructor(data) {
-    this.draw = this.draw.bind(this);
+  constructor( data ) {
+    this.draw = this.draw.bind( this );
     this.data = data
     this.gridScale = 5
     this.gridColor = "#eeeeee"
@@ -112,7 +106,7 @@ class BarChart {
   }
 
   draw () {
-    requestAnimationFrame(this.draw)
+    //requestAnimationFrame(this.draw)
 
     const { data, gridScale, gridColor, distanceBars, padding } = this
     
@@ -123,15 +117,15 @@ class BarChart {
     const canvasActualWidth = canvas.width - padding.left - padding.right
 
     let maxValue = 0;
-    for (const index in data){
+    for ( const index in data ) {
       maxValue = Math.max(maxValue, data[index].value);
     }
 
     //drawing the grid lines
     let gridValue = 0;
-    while (gridValue <= maxValue){
-      var gridY = canvasActualHeight * (1 - gridValue / maxValue) + padding.top;
-      const line = new Line(0, gridY, canvas.width, gridY, gridColor)
+    while ( gridValue <= maxValue ) {
+      var gridY = canvasActualHeight * ( 1 - gridValue / maxValue ) + padding.top;
+      const line = new Line( 0, gridY, canvas.width, gridY, gridColor )
       line.draw(context)
       gridValue += gridScale;
     }
@@ -141,7 +135,7 @@ class BarChart {
     const numberOfBars = Object.keys(data).length
     const barSize = canvasActualWidth / numberOfBars
 
-    for (const index in data){
+    for (const index in data) {
       const value = data[index].value
       const barHeight = Math.round( canvasActualHeight * value / maxValue)
       
@@ -149,13 +143,13 @@ class BarChart {
       const yBar = canvas.height - barHeight - padding.bottom
 
       const bar = new Rectangle(xBar, yBar, barSize - distanceBars, barHeight, data[index].color)
-      bar.draw(context)
+      bar.draw( context )
 
-      const xText = barIndex * barSize + (barSize / 2)
+      const xText = barIndex * barSize + ( barSize / 2 )
       const yText = canvas.height - barHeight - padding.bottom + barHeight
 
-      const text = new Text(data[index].name, xText, yText);
-      text.draw(context)
+      const text = new Text( data[index].name, xText, yText );
+      text.draw( context )
 
       barIndex++;
     }
@@ -186,8 +180,8 @@ let myData = [
   }
 ]
 
-const canvas = document.getElementById('canvas')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById( 'canvas' )
+const context = canvas.getContext( '2d' )
 
-const chart = new BarChart(myData)
+const chart = new BarChart( myData )
 chart.draw();
